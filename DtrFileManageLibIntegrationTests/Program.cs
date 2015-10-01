ca72a7f51a97ac72e9608ada0be7d5506732a06a
Борхаленко Олег <borkhalenko@hotmail.com>
2015-10-01 12:18:24 +0300

    Created new project DtrFileManageLibIntegrationTests. Also changed .net framework version to the DtrFileManageLib project.

---
+++ DtrFileManageLibIntegrationTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DtrFileManageLib;
+
+namespace DtrFileManageLibIntegrationTests {
+    class Program {
+        static void Main(string[] args) {
+            FSMonitor monitor = FSMonitor.GetInstance("C:/test");
+            monitor.StartWatch();
+            System.Threading.Thread.Sleep(10000000);
+        }
+    }
+}
