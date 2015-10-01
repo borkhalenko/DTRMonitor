1d3f1e65d7681c449fdad68de949cdd84382fafc
Борхаленко Олег <borkhalenko@hotmail.com>
2015-10-01 12:13:32 +0300

    Added new project DtrFileManageLib.

---
+++ DtrFileManageLib/FSMonitor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtrFileManageLib {
+    public delegate void FileChanged(string fileName, DateTime dateTime);
+    public delegate void ErrorOccured(string errorMessage, DateTime dateTime);
+    public sealed class FSMonitor {
+        private static Dictionary<string, FSMonitor> monitors=new Dictionary<string, FSMonitor>();
+        private FileSystemWatcher fsWatcher;
+        public event FileChanged FileCreated;
+        public event FileChanged FileModified;
+        public event FileChanged FileDeleted;
+        private FSMonitor(string newPath) {
+            fsWatcher = new FileSystemWatcher();
+            fsWatcher.Path=newPath;
+            fsWatcher.Created += FsWatcher_Created;
+            fsWatcher.Deleted += FsWatcher_Deleted;
+            fsWatcher.Changed += FsWatcher_Changed;
+        }
+
+        private void FsWatcher_Changed(object sender, FileSystemEventArgs e) {
+            Console.WriteLine("File {0} was changed.", e.Name);
+            Console.WriteLine("Hashsum={0}", FileAttributes.ComputeHash(e.FullPath));
+        }
+
+        private void FsWatcher_Deleted(object sender, FileSystemEventArgs e) {
+            Console.WriteLine("File {0} was deleted.", e.Name);
+        }
+
+        private void FsWatcher_Created(object sender, FileSystemEventArgs e) {
+            Console.WriteLine("File {0} was created.", e.Name);
+            Console.WriteLine("Hashsum={0}", FileAttributes.ComputeHash(e.FullPath));
+        }
+
+        public void StartWatch() {
+            fsWatcher.EnableRaisingEvents = true;
+        }
+
+        public void StopWatch() {
+            fsWatcher.EnableRaisingEvents = false;
+        }
+
+        public static FSMonitor GetInstance(string path) {
+            if (monitors.ContainsKey(path)) {
+                return monitors[path];
+            }
+            else {
+                FSMonitor newItem = new FSMonitor(path);
+                monitors.Add(path, newItem);
+                return newItem;
+            } 
+        }
+    }
+}
---
+++ DtrFileManageLib/FileAttributes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace DtrFileManageLib {
+    public static class FileAttributes {
+        public static string ComputeHash(string fileName) {
+            using (var md5 = MD5.Create()) {
+                using (var stream=File.OpenRead(fileName)){
+                    return BitConverter.ToString(md5.ComputeHash(stream));
+                }
+            }
+        }
+    }
+}
