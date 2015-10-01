902efd951a71b4b5d42b14c92613f0b7487fdf4a
Борхаленко Олег <borkhalenko@hotmail.com>
2015-10-01 12:07:54 +0300

    Created new solution DtrMonitor. Added new project DtrMonitorCore. Added new interface IDtrMonitorService (service contract for WCF) and implementation - class FileTransferService.

---
+++ DtrMonitorCore/FileTransferService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DtrMonitorCore {
+    class FileTransferService : IDtrMonitorService {
+        public RemoteFileStream DownloadFile(RemoteFileInfo fi) {
+            throw new NotImplementedException();
+        }
+
+        public RemoteFileInfo isNext() {
+            throw new NotImplementedException();
+        }
+
+        public RemoteFileInfo UploadFile(RemoteFileStream fs) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DtrMonitorCore/IDtrMonitorService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace DtrMonitorCore {
+    [ServiceContract]
+    public interface IDtrMonitorService {
+        [OperationContract]
+        RemoteFileInfo isNext();
+        [OperationContract]
+        RemoteFileStream DownloadFile(RemoteFileInfo fi);
+        [OperationContract]
+        RemoteFileInfo UploadFile(RemoteFileStream fs);
+    }
+
+    [MessageContract]
+    public class RemoteFileInfo {
+        [MessageBodyMember]
+        public bool fileExists;
+        [MessageBodyMember]
+        public string fileName;
+        [MessageBodyMember]
+        public string fileHash;
+    }
+
+    [MessageContract]
+    public class RemoteFileStream {
+        [MessageHeader(MustUnderstand = true)]
+        public RemoteFileInfo fileInfo;
+        [MessageHeader(MustUnderstand = true)]
+        public UInt64 length;
+        [MessageBodyMember]
+        public System.IO.Stream FileByteStream;
+        public void Dispose() {
+            if (FileByteStream != null) {
+                FileByteStream.Close();
+                FileByteStream = null;
+            }
+        }
+    }
+}
